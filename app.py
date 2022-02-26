198929eec95df14d5d0abea718730cd5a58699a8
DellFair <rfair3@student.gsu.edu>
2022-02-25 20:48:09 -0500

    Created database for username and password. Now creating forms for username and password. Also working on register form.

--- app.py
+++ app.py
@@ -1,14 +1,55 @@
+from wsgiref.validate import validator
 import requests
 import os
 import random
 import base64
 
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
 from wikipedia import get_wiki_link
 from tmdb import get_movie_data
 
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' 
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[ InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Register")
+
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                "Opps. Someone already has that username. Please choose a different one.")
+
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[ InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Login")
+
 
 
 MOVIE_IDS = [
@@ -20,18 +61,21 @@ MOVIE_IDS = [
 ]
 
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
-    
-
-@app.route("/register")
-def register():
-    return render_template("register.html")
-    
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template("login.html", form=form)
+    
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form= RegisterForm()
+    return render_template("register.html", form=form)
     
 
 
